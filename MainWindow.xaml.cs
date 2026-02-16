e0f58151a53b55a11ced1e3545cab956e71eef2d
VishalKapur <vishalkapur13@gmail.com>
2026-02-16 16:32:56 +0000

    Implementing countdown timer
    DispatchTimer is created to countdown every second and update the countdown textbox
    
    Shutdown command is run when timer ends
    
    Cancel button stops the timer

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Configuration;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WindowsShutdownTimer
 {
@@ -17,6 +19,9 @@ namespace WindowsShutdownTimer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer = new DispatcherTimer();
+        private int seconds;    // Seconds left on timer
+
         public MainWindow()
         {
             InitializeComponent();
@@ -26,13 +31,37 @@ namespace WindowsShutdownTimer
         {
             if (!int.TryParse(HoursTextBox.Text, out int hours) || !int.TryParse(MinutesTextBox.Text, out int minutes))
                 MessageBox.Show("Please enter a valid number", "Error", MessageBoxButton.OK, MessageBoxImage.Error);    // Shows error if either box does not contain a number
-            else;
-                // Begin timer
+            else
+                CreateCountdown(hours, minutes);
         }
 
         private void Cancel_Button_Click(object sender, RoutedEventArgs e)
         {
+            timer.Stop();
+            CountdownText.Text = TimeSpan.FromSeconds(0).ToString();
+        }
 
+        private void CreateCountdown(int hours, int minutes)
+        {
+            seconds = (hours * 3600) + (minutes * 60);
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += Timer_Tick;
+            CountdownText.Text = TimeSpan.FromSeconds(seconds).ToString();
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (seconds > 0)
+            {
+                seconds--;
+                CountdownText.Text = TimeSpan.FromSeconds(seconds).ToString();
+            }
+            else
+            {
+                timer.Stop();
+                Process.Start("shutdown", "/s /t 0");
+            }
         }
     }
 }
